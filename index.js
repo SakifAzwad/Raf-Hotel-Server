33e321e3bfd3fc984b1ee1531449e0ff2036c3cb
SakifAzwad <sakifazwad18@gmail.com>
2023-11-08 13:57:06 +0600

    Added rooms api

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gp9ypzc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,14 +27,23 @@ async function run() {
     const rafhotelCol = client.db("raf-hotel").collection("rooms");
 
     app.get("/rooms", async (req, res) => {
-        const cursor = rafhotelCol.find();
-        const rest = await cursor.toArray();
-        res.send(rest);
-      });
+      const cursor = rafhotelCol.find();
+      const rest = await cursor.toArray();
+      res.send(rest);
+    });
+
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await rafhotelCol.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,11 +51,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("server running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Coffee server is running on port:${port}`);
-  });
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log(`Coffee server is running on port:${port}`);
+});
