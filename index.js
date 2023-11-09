2bffc08b2416d4236e8fa4459e453ce15b844f85
SakifAzwad <sakifazwad18@gmail.com>
2023-11-09 23:58:55 +0600

    Added Final Touch

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use();
+app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
